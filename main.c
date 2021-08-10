4e4fdf8842defd0a8897d5038f894cc9a1c4a9d9
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-10 14:56:46 -0300

    Menu de operacoes

--- main.c
+++ main.c
@@ -2,28 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main() {
   Fila *f = criar();
-  Fila *f2 = criar();
 
-  inserir(f, 10);
-  inserir(f, 11);
-  inserir(f, 13);
+  int op, tempoDeExpediente;
 
-	consultar(f);
+  printf("Qual é o tempo de expediente?\n");
+  scanf("%d", &tempoDeExpediente);
 
-  // printf("\n Quantidade de elementos: %d", contar(f));
+  do {
+    printf("\n----------Fila do banco----------\n");
+    printf("\n[1] - Consultar saldo");
+    printf("\n[2] - Sacar");
+    printf("\n[3] - Aplicação");
+    printf("\n[4] - Extrato");
+    printf("\n[5] - Pagamento em dinheiro");
+    printf("\n[6] - Pagamento com débito em conta");
+    printf("\n[7] - exibir dados diários");
+    printf("\n[0] - SAIR");
+    printf("\n---------------------------------\n");
+    printf("Escolha um serviço:\n");
+    scanf("%d", &op);
 
-  // consultar(f);
-
-  // remover(f);
-  // remover(f);
-
-	// removeNoFinal(f, 13);
-
-	//inseriNoInicio(f, 14);
-
-  // consultar(f);
+    switch (op) {
+      case 1:
+        // consultarSaldo();
+        // calcularTempoDeEspera();
+        break;
+      case 2:
+        // sacar();
+        // calcularTempoDeEspera();
+        break;
+      case 3:
+        // aplicacao();
+        // calcularTempoDeEspera();
+        break;
+      case 4:
+        // extrato();
+        // calcularTempoDeEspera();
+        break;
+      case 5:
+        // pagamentoEmDinheiro();
+        // calcularTempoDeEspera();
+        break;
+      case 6:
+        // pagamentoComDebito();
+        // calcularTempoDeEspera();
+        break;
+      case 7:
+        printf("\nFim do expediente:\n");
+        // clientesAtendidos();
+        // clientesNaoAtendidos();
+        // tempoPrioridade();
+        // quantidadePorOperacao();
+        // tempoTotalPorOperacao();
+        break;
+      case 0:
+        printf("\nTransações finalizadas\n");
+        break;
+      default:
+        printf("\nOperação inválida\n");
+        break;
+    }
+  } while (op != 0);
 
   return 0;
 }
